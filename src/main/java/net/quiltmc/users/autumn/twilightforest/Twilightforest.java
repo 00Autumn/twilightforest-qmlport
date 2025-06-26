af796623095d95baf9dbde1b7c5ad9f44ad7975b
autumn <tsukibusiness@protonmail.com>
2025-06-26 06:56:02 -0400

    initial

---
+++ src/main/java/net/quiltmc/users/autumn/twilightforest/Twilightforest.java
@@ -0,0 +1,16 @@
+package net.quiltmc.users.autumn.twilightforest;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Twilightforest implements ModInitializer {
+	public static final String MOD_ID = "twilightforest";
+    public static final Logger LOGGER = LoggerFactory.getLogger("twilightforest");
+
+    @Override
+    public void onInitialize(ModContainer mod) {
+
+    }
+}
---
+++ src/main/java/net/quiltmc/users/autumn/twilightforest/TwilightforestClient.java
@@ -0,0 +1,11 @@
+package net.quiltmc.users.autumn.twilightforest;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+
+public class TwilightforestClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient(ModContainer modContainer) {
+
+	}
+}
